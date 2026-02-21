2691715f387afbc2e933e3efd44859eacfc2d5a7
hubbylei <hubbylei@hubbylei.dynu.net>
2026-02-22 00:27:38 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -45,9 +45,9 @@ sed -i 's/GEOSITE_VER:=.*/GEOSITE_VER:='"${GEOSITE_VER}"'/g' package/custom/v2ra
 sed -i '/FILE:=$(GEOSITE_FILE)/a\ HASH:='"${GEOSITE_HASH}"'' package/custom/v2ray-geodata/Makefile
 sed -i 's/URL:=https:\/\/www.v2fly.org/URL:=https:\/\/github.com\/Loyalsoldier\/v2ray-rules-dat/g' package/custom/v2ray-geodata/Makefile
 
-SMARTDNS_JSON=$(curl -sL -H "${AUTH}" https://api.github.com/repos/pymumu/smartdns/commits)
-SMARTDNS_VER=$(echo -n `echo ${SMARTDNS_JSON} | jq -r .[0].commit.committer.date | awk -F "T" '{print $1}' | sed 's/\-/\./g'`)
-SMARTDNS_SHA=$(echo -n `echo ${SMARTDNS_JSON} | jq -r .[0].sha`)
+SMARTDNS_JSON=$(curl -sL -H "${AUTH}" https://api.github.com/repos/pymumu/smartdns/commits | jq .[0])
+SMARTDNS_VER=$(echo -n `echo ${SMARTDNS_JSON} | jq -r .commit.committer.date | awk -F "T" '{print $1}' | sed 's/\-/\./g'`)
+SMARTDNS_SHA=$(echo -n `echo ${SMARTDNS_JSON} | jq -r .sha`)
 sed -i '/PKG_MIRROR_HASH:=/d' package/custom/smartdns/Makefile
 sed -i 's/PKG_VERSION:=.*/PKG_VERSION:='"${SMARTDNS_VER}"'/g' package/custom/smartdns/Makefile
 sed -i 's/PKG_SOURCE_VERSION:=.*/PKG_SOURCE_VERSION:='"${SMARTDNS_SHA}"'/g' package/custom/smartdns/Makefile
